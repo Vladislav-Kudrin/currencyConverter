e769c0f5d05b8934011afdf3c84eeb16925a94c9
= <vladislav2001vlad26@gmail.com>
2022-07-14 00:40:22 +0300

    APIConnect is separated from MainActivity class.

---
+++ app/src/main/java/com/example/currencyconverter/APIConnection.java
@@ -0,0 +1,153 @@
+package com.example.currencyconverter;
+
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Gets and initializes default exchange currencies' rates or from an exchange rates API server.
+ *
+ * @author Vladislav
+ * @version 1.2
+ * @since 1.0
+ */
+final class APIConnection implements Runnable {
+    /**
+     * A modifiable set of currency/rate mappings from a submitter main activity class.
+     */
+    private static JSONObject jsonObject;
+    /**
+     * A modifiable set of currency/rate mappings from.
+     */
+    private static JSONObject conversionRates;
+    /**
+     * Submitter main activity class.
+     */
+    private static MainActivity mainActivity;
+    /**
+     * A JSON response from an exchange rates API server.
+     */
+    private static String json;
+    /**
+     * {@code jsonObject} key name.
+     */
+    private static final String CONVERSION_RATES = "conversion_rates";
+
+    public APIConnection(MainActivity mainActivity, JSONObject jsonObject) {
+        APIConnection.mainActivity = mainActivity;
+        APIConnection.jsonObject = jsonObject;
+    }
+
+    /**
+     * Initializes default exchange currencies' rates and tries to connect to an exchange rates
+     * API server.
+     *
+     * @author Vladislav
+     * @since 1.0
+     */
+    @Override
+    public final void run() {
+        try {
+            conversionRates = new JSONObject("{\"RUB\":1,\"USD\":1," +
+                    "\"EUR\":1,\"JPY\":1,\"AUD\":1,\"AZN\":1,\"GBP\":1,\"AMD\":1,\"BYN\":1," +
+                    "\"BGN\":1}}");
+            jsonObject.put(CONVERSION_RATES, conversionRates);
+
+            mainActivity.getRates();
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
+
+        if (isOnline())
+            apiConnect();
+
+        updateUi();
+    }
+
+    /**
+     * Checks for internet access.
+     * Returns true if there is internet access and false otherwise.
+     *
+     * @return internet access availability.
+     *
+     * @author Vladislav
+     * @since 1.1
+     */
+    private boolean isOnline() {
+        try {
+            final Socket socket = new Socket();
+            final SocketAddress socketAddress = new InetSocketAddress("8.8.8.8", 53);
+
+            socket.connect(socketAddress, 1500);
+            socket.close();
+
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Initializes {@code json} with exchange rates from an exchange rates' API server if a
+     * response is received.
+     *
+     * @author Vladislav
+     * @since 1.0
+     */
+    private void apiConnect() {
+        try {
+            String line;
+            final URL url = new URL("https://v6.exchangerate-api.com/v6/" + APIConfig.CODE +
+                    "/latest/RUB");
+            final URLConnection urlConnection = url.openConnection();
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    urlConnection.getInputStream()));
+
+            json = "";
+
+            while ((line = bufferedReader.readLine()) != null)
+                json = json.concat(line);
+
+            bufferedReader.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates exchange currencies' tiles' values.
+     *
+     * @author Vladislav
+     * @since 1.1
+     */
+    private void updateUi() {
+        mainActivity.runOnUiThread(() -> {
+            if (json == null)
+                Toast.makeText(mainActivity.getApplicationContext(),
+                        mainActivity.getResources().getString(R.string.no_internet_access),
+                        Toast.LENGTH_SHORT).show();
+            else if (json.length() != 0)
+                try {
+                    conversionRates = new JSONObject(json);
+
+                    jsonObject.put(CONVERSION_RATES,
+                            conversionRates.getJSONObject(CONVERSION_RATES));
+                    mainActivity.getRates();
+                } catch (JSONException exception) {
+                    exception.printStackTrace();
+                }
+            else
+                Toast.makeText(mainActivity.getApplicationContext(),
+                        mainActivity.getResources().getString(R.string.failed_connection),
+                        Toast.LENGTH_SHORT).show();
+        });
+    }
+}
--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -8,18 +8,9 @@ import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.URL;
-import java.net.URLConnection;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -32,19 +23,15 @@ import org.json.JSONObject;
  * Runs the main application activity.
  *
  * @author Vladislav
- * @version 1.1
+ * @version 1.2
  * @since 1.0
  */
-public final class MainActivity extends AppCompatActivity implements
+public class MainActivity extends AppCompatActivity implements
         ExchangeCurrencySelection.SelectionListener {
     /**
      * An instance of a converter activity.
      */
     private static Intent converterActivity;
-    /**
-     * A modifiable set of currency/rate mappings.
-     */
-    private static JSONObject jsonObject;
     /**
      * A user's locale region.
      */
@@ -54,9 +41,9 @@ public final class MainActivity extends AppCompatActivity implements
      */
     private static String currentDate;
     /**
-     * A JSON response from an exchange rates' API.
+     * A modifiable set of currency/rate mappings.
      */
-    private static String json;
+    private static JSONObject jsonObject = new JSONObject();
     /**
      * A selected exchange currency's code.
      */
@@ -73,6 +60,10 @@ public final class MainActivity extends AppCompatActivity implements
      * An exchange currencies' rates' array.
      */
     private static final double[] RATES = new double[10];
+    /**
+     * {@code jsonObject} key name.
+     */
+    private static final String CONVERSION_RATES = "conversion_rates";
 
     /**
      * Creates the main application activity.
@@ -99,7 +90,7 @@ public final class MainActivity extends AppCompatActivity implements
         ((Button) findViewById(R.id.dateButton)).setText(currentDate);
 
         if (savedInstanceState == null) {
-            executorService.submit(new APIConnection());
+            executorService.submit(new APIConnection(MainActivity.this, jsonObject));
             executorService.shutdown();
         }
 
@@ -121,7 +112,6 @@ public final class MainActivity extends AppCompatActivity implements
     protected final void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        json = savedInstanceState.getString("JSON");
         currentDate = savedInstanceState.getString("CURRENT_DATE");
         exchangeCurrency = savedInstanceState.getString("EXCHANGE_CURRENCY");
         exchangeCurrencySign = savedInstanceState.getString("EXCHANGE_CURRENCY_SIGN");
@@ -129,7 +119,7 @@ public final class MainActivity extends AppCompatActivity implements
         ((Button) findViewById(R.id.dateButton)).setText(currentDate);
 
         try {
-            jsonObject = new JSONObject(json);
+            jsonObject = new JSONObject(savedInstanceState.getString("JSON"));
 
             getRates();
         } catch (JSONException e) {
@@ -147,7 +137,7 @@ public final class MainActivity extends AppCompatActivity implements
      */
     @Override
     protected final void onSaveInstanceState(Bundle outState) {
-        outState.putString("JSON", json);
+        outState.putString("JSON", jsonObject.toString());
         outState.putString("CURRENT_DATE", currentDate);
         outState.putString("EXCHANGE_CURRENCY", exchangeCurrency);
         outState.putString("EXCHANGE_CURRENCY_SIGN", exchangeCurrencySign);
@@ -189,13 +179,13 @@ public final class MainActivity extends AppCompatActivity implements
      * @author Vladislav
      * @since 1.0
      */
-    private void getRates() {
+    final void getRates() {
         try {
-            final double selectedRate = jsonObject.getJSONObject("conversion_rates")
+            final double selectedRate = jsonObject.getJSONObject(CONVERSION_RATES)
                     .getDouble(exchangeCurrency);
 
             for (int index = 0; index < 10; index++) {
-                RATES[index] = selectedRate / jsonObject.getJSONObject("conversion_rates")
+                RATES[index] = selectedRate / jsonObject.getJSONObject(CONVERSION_RATES)
                         .getDouble(getResources().getStringArray(R.array.currencies)[index]);
             }
 
@@ -228,6 +218,9 @@ public final class MainActivity extends AppCompatActivity implements
      *
      * @param index an index of the currency tile.
      * @return a formatted title of the currency tile.
+     *
+     * @author Vladislav
+     * @since 1.0
      */
     private SpannableString getSpan(int index) {
         int length;
@@ -278,116 +271,5 @@ public final class MainActivity extends AppCompatActivity implements
         converterActivity.putExtra("EXCHANGE_CURRENCY_SIGN", exchangeCurrencySign);
 
         startActivity(converterActivity);
-    }
-
-    /**
-     * Gets and initializes default exchange currencies' rates or from an exchange rates API server.
-     *
-     * @author Vladislav
-     * @version 1.1
-     * @since 1.0
-     */
-    private final class APIConnection implements Runnable {
-        /**
-         * Initializes default exchange currencies' rates and tries to connect to an exchange rates
-         * API server.
-         *
-         * @author Vladislav
-         * @since 1.0
-         */
-        @Override
-        public final void run() {
-            try {
-                jsonObject = new JSONObject("{\"conversion_rates\":{\"RUB\":1,\"USD\":1," +
-                        "\"EUR\":1,\"JPY\":1,\"AUD\":1,\"AZN\":1,\"GBP\":1,\"AMD\":1,\"BYN\":1," +
-                        "\"BGN\":1}}");
-
-                getRates();
-            } catch (JSONException exception) {
-                exception.printStackTrace();
-            }
-
-            if (isOnline())
-                apiConnect();
-
-            updateUi();
-        }
-
-        /**
-         * Checks for internet access.
-         * Returns true if there is internet access and false otherwise.
-         *
-         * @return internet access availability.
-         *
-         * @author Vladislav
-         * @since 1.1
-         */
-        private boolean isOnline() {
-            try {
-                final Socket socket = new Socket();
-                final SocketAddress socketAddress = new InetSocketAddress("8.8.8.8", 53);
-
-                socket.connect(socketAddress, 1500);
-                socket.close();
-
-                return true;
-            } catch (IOException e) {
-                return false;
-            }
-        }
-
-        /**
-         * Initializes {@code json} with exchange rates from an exchange rates' API server if a
-         * response is received.
-         *
-         * @author Vladislav
-         * @since 1.0
-         */
-        private void apiConnect() {
-            try {
-                String line;
-                final URL url = new URL("https://v6.exchangerate-api.com/v6/" + APIConfig.CODE +
-                        "/latest/" + exchangeCurrency);
-                final URLConnection urlConnection = url.openConnection();
-                final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                        urlConnection.getInputStream()));
-
-                json = "";
-
-                while ((line = bufferedReader.readLine()) != null)
-                    json = json.concat(line);
-
-                bufferedReader.close();
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-
-        /**
-         * Updates exchange currencies' tiles' values.
-         *
-         * @author Vladislav
-         * @since 1.1
-         */
-        private void updateUi() {
-            runOnUiThread(() -> {
-                if (json == null)
-                    Toast.makeText(getApplicationContext(),
-                            getResources().getString(R.string.no_internet_access),
-                            Toast.LENGTH_SHORT).show();
-                else if (json.length() != 0)
-                    try {
-                        jsonObject = new JSONObject(json);
-
-                        getRates();
-                    } catch (JSONException exception) {
-                        exception.printStackTrace();
-                    }
-                else
-                    Toast.makeText(getApplicationContext(),
-                            getResources().getString(R.string.failed_connection),
-                            Toast.LENGTH_SHORT).show();
-            });
-        }
     }
 }
